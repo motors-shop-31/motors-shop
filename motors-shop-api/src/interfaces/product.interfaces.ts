1aab58d394ee01f6faa63f8dfa019ba005ff8f31
Jasmynels <jasmyneleandro0@gmail.com>
2023-02-22 23:39:58 -0300

    feat: create interface products

--- motors-shop-api/src/interfaces/product.interfaces.ts
+++ motors-shop-api/src/interfaces/product.interfaces.ts
@@ -1,3 +1,5 @@
+import { IUser } from "./user.interfaces";
+
 interface IProductCreate {
   type: string;
   title: string;
@@ -11,4 +13,40 @@ interface IProductCreate {
   image: string[];
 }
 
-export { IProductCreate };
+interface IProduct{
+  user: IUser,
+  type: string;
+  title: string;
+  year: number;
+  mileage: number;
+  price: number;
+  description: string;
+  vehicle: string;
+  published: boolean;
+  cover_image: string;
+  // image: string[];
+}
+interface IProductUpdate{
+  type?: string;
+  title?: string;
+  year?: number;
+  mileage?: number;
+  price?: number;
+  description?: string;
+  vehicle?: string;
+  published?: boolean;
+  cover_image?: string;
+  // image: string[];
+}
+
+interface IProductList {
+  message: string,
+  data: IProduct[]
+}
+
+interface IProductResponse {
+  message: string,
+  data: IProductUpdate
+}
+
+export { IProductCreate, IProduct, IProductList, IProductUpdate,IProductResponse };
