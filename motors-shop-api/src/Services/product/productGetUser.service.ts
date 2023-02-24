074a5e3b7a51084b123704d7b4bd3431a81d1587
Jasmynels <jasmyneleandro0@gmail.com>
2023-02-24 16:10:44 -0300

    feat: create service get user

--- motors-shop-api/src/Services/product/productGetUser.service.ts
+++ motors-shop-api/src/Services/product/productGetUser.service.ts
@@ -2,25 +2,25 @@ import AppDataSource from "../../data-source";
 import { Product } from "../../entities/product.entity";
 import { User } from "../../entities/user.entity";
 import { AppError } from "../../errors/appError";
-import { IProductList } from "../../interfaces/product.interfaces";
+import {  IProductList } from "../../interfaces/product.interfaces";
 
-const productGetUserService = async (user_id: string): Promise<IProductList> => {
+const productGetUserService = async (userId: string): Promise<IProductList> => {
     const productRepository = AppDataSource.getRepository(Product);
     const userRepository = AppDataSource.getRepository(User)
-    const user = await userRepository.findOneBy({id : user_id})
-    const products = await productRepository.find({relations : {user : true}})
+    const user = await userRepository.findOneBy({id : userId})
+    const products = await productRepository.find({relations : {user : false}})
 
     if (!user){
         throw new AppError(400,"User not found")
     }
     if (!products){
-        throw new AppError(400,"Cart not found")
+        throw new AppError(400,"Products not found")
     }
 
     return {
-        message: "listed all products for user",
+        message: "listen",
         data: products
-    };
+    }
 };
 
 export default productGetUserService;
