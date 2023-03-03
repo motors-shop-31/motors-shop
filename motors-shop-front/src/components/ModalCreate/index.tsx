ca88e705d540a057e32ee86d5bc1115f043fa568
Hyaguinhogp <hyago.v.a.m@gmail.com>
2023-03-03 14:21:40 -0400

    feat: create product styled

--- motors-shop-front/src/components/ModalCreate/index.tsx
+++ motors-shop-front/src/components/ModalCreate/index.tsx
@@ -1,116 +1,184 @@
 // import * as yup from "yup";
-import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import React, { useContext } from 'react'
-import { Input } from '../Input'
-import { ButtonFooter, Buttons, FormMain, InputsInfos, Modal, ModalBody, ModalHeader } from './style';
-import { IPostResponse, ValuesFunctions } from '../../contexts/InputsContext';
-import { ModalHeaderStryled } from '../ModalSucess/style';
+import { useContext, useState } from 'react';
+import { useForm } from "react-hook-form";
+import { FieldValues } from "react-hook-form/dist/types";
+import { GrFormClose } from "react-icons/gr";
+import * as yup from "yup";
+
+import { IPostResponse } from '../../contexts/InputsContext';
 import { AuthContext } from '../../contexts/modalContext';
-import { ModalCloseButton } from "@chakra-ui/react";
-import  { GrFormClose } from "react-icons/gr"
+import api from "../../service/api";
+import Options from "../Options";
+import {
+  BottomButtons,
+  DefaultInputContainer,
+  InputContainer02,
+  InputContainerButtons,
+  InputContainerMarginBottom,
+  Modal,
+  ModalBody,
+  ModalFormContainer,
+  ModalHeader
+} from './style';
 
-const ModalForm = () =>  {
-    const { formSchema, onSubmitFunction } = useContext(ValuesFunctions);
-    const { modal, openModal, closeModal } = useContext(AuthContext);
-  
-    const {
-        register,
-        handleSubmit,
-        // formState: { errors }
-      } = useForm<IPostResponse>({
-        resolver: yupResolver(formSchema)
-      });
+const ModalForm = () => {
+  const { modal, openModal, closeModal } = useContext(AuthContext);
+  const [imagesURLs, setImagesURLs] = useState([""]);
 
-    return modal === true ? (
-      <> 
+  const addImage = () => {
+    setImagesURLs([...imagesURLs, ""]);
+  };
+
+  const updateImage = (index: number, url: string) => {
+    const newImages = [...imagesURLs];
+    newImages[index] = url;
+    setImagesURLs(newImages);
+  };
+
+  const formSchema = yup.object().shape({
+    type: yup.string().required("Tipo obrigratório"),
+    title: yup.string().required("Título obrigatório"),
+    year: yup.string().required("Ano obrigatório"),
+    mileage: yup.string().required("Kilometragem obrigatória"),
+    price: yup.number().required("Preço obrigatório"),
+    description: yup.string().required("Descrição obrigatória"),
+    vehicle: yup.string().required("Tipo do veículo obrigatório"),
+    cover_image: yup.string().required("Url da imagem obrigatória"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<IPostResponse>({
+    resolver: yupResolver(formSchema)
+  });
+
+  const onSubmitFunction = (data: FieldValues) => {
+    const request = { ...data, images: imagesURLs }
+    console.log(request)
+  };
+
+  return (
+    <>
       <Modal>
         <ModalBody>
-        <ModalHeader>
-          <ModalHeaderStryled>Criar anuncio</ModalHeaderStryled>
-          <GrFormClose id='xis' onClick={() => closeModal()}/>
-        </ModalHeader>
-        <div>
-          <FormMain onSubmit={handleSubmit(onSubmitFunction)}>
-          <label className='label'>Tipo do anuncio</label>
-                  <Buttons>
-                      <button className='button1'>Venda</button>
-                      <button className='button2'>Leilão</button>
-                  </Buttons>
-                  <label className='label'>Informações de veículo</label>
-                  <Input label='Título' placeholder='Mercedes Benz A 200 CGI ADVANCE SEDAN' {...register("title")}/>
-                  <InputsInfos>
-                      <Input label='Ano' placeholder='2018' {...register("year")}/>
-                      <Input label='Quilometragem' placeholder='0' {...register("mileage")}/>
-                      <Input label='Preço' placeholder='50.000.00' {...register("price")}/>
-                  </InputsInfos>
-                  <Input  label='Descrição'  {...register("description")} placeholder='Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s'/> 
-                  <label className='label'>Tipo de veículo</label>
-                  <Buttons>
-                      <button className='button1'>Carro</button>
-                      <button className='button2'>Moto</button>
-                  </Buttons> 
-                  <Input label='Imagem de capa' placeholder='https://image.com' {...register("cover_image")}/>         
-                  <Input label='1º imagem da galeria' placeholder='https://image.com' {...register("image")}/>         
-                  <Input label='2º imagem da galeria' placeholder='https://image.com' {...register("image")}/>  
-                  <button id='adcCampo'>Adicionar campo para imagem da galeria</button> 
-                  <ButtonFooter>
-                      <button className='cancel' onClick={() => closeModal()}>Cancelar</button>
-                      <button type='submit' className='enter'>Criar anuncio</button>
-                  </ButtonFooter>
-          </FormMain>
-        </div>
+          <ModalHeader>
+            <h1 className="modal_title">Criar anuncio</h1>
+            <GrFormClose id='xis' onClick={() => closeModal()} />
+          </ModalHeader>
+          <ModalFormContainer onSubmit={handleSubmit(onSubmitFunction)}>
+            <InputContainerButtons>
+              <h2 className="body-2-500">Tipo de anúncio</h2>
+              <Options
+                options={[
+                  {
+                    htmlFor: "Venda",
+                    fieldValue: "sale",
+                  },
+                  {
+                    htmlFor: "Leilão",
+                    fieldValue: "auction",
+                  },
+                ]}
+                fieldName="type"
+                register={register}
+              />
+              <span>{errors?.type?.message}</span>
+            </InputContainerButtons>
+
+            <DefaultInputContainer>
+              <h2 className="body-2-500">Título</h2>
+              <input type="text" placeholder="Digitar título" {...register("title")} />
+              <span>{errors?.title?.message}</span>
+            </DefaultInputContainer>
+
+            <InputContainer02>
+              <DefaultInputContainer>
+                <h2 className="body-2-500">Ano</h2>
+                <input type="text" placeholder="2018" {...register("year")} />
+                <span>{errors?.year?.message}</span>
+              </DefaultInputContainer>
+              <DefaultInputContainer>
+                <h2 className="body-2-500">Quilometragem</h2>
+                <input type="text" placeholder="0" {...register("mileage")} />
+                <span>{errors?.mileage?.message}</span>
+              </DefaultInputContainer>
+            </InputContainer02>
+
+            <DefaultInputContainer>
+              <h2 className="body-2-500">Preço</h2>
+              <input type="text" placeholder="50.000,00" {...register("price")} />
+              <span>{errors?.price?.message}</span>
+            </DefaultInputContainer>
+
+            <DefaultInputContainer>
+              <h2 className="body-2-500">Descrição</h2>
+              <textarea id="" placeholder="Digitar Descrição" {...register("description")}></textarea>
+              <span>{errors?.description?.message}</span>
+            </DefaultInputContainer>
+
+            <InputContainerButtons>
+              <h2 className="body-2-500">Tipo de veículo</h2>
+              <Options
+                options={[
+                  {
+                    htmlFor: "Carro",
+                    fieldValue: "car",
+                  },
+                  {
+                    htmlFor: "Moto",
+                    fieldValue: "motocycle",
+                  },
+                ]}
+                fieldName="vehicle"
+                register={register}
+              />
+              <span>{errors?.vehicle?.message}</span>
+            </InputContainerButtons>
+
+            <DefaultInputContainer>
+              <h2 className="body-2-500">Imagem da capa</h2>
+              <input id="" placeholder="https://image.com" {...register("cover_image")} />
+              <span>{errors?.cover_image?.message}</span>
+            </DefaultInputContainer>
+
+            {imagesURLs.map((url, index) => {
+              return (
+                <InputContainerMarginBottom key={index}>
+                  <h2 className="body-2-500">{index + 1 + "° Imagem da galeria"}</h2>
+                  <input
+                    placeholder="https://image.com"
+                    onChange={(event) => {
+                      updateImage(index, event.target.value)
+                    }}
+                  />
+                </InputContainerMarginBottom>
+              );
+            })}
+
+            <button className="medium BrandOpacity" onClick={(e) => {
+              e.preventDefault()
+              addImage()
+            }}>
+              Adicionar campo para imagem da galeria
+            </button>
+
+            <BottomButtons>
+              <button className="big negative" onClick={(e) => {
+                e.preventDefault()
+                closeModal()
+              }}>Cancelar</button>
+              <button className="big brand1">Criar anúncio</button>
+            </BottomButtons>
+
+          </ModalFormContainer>
         </ModalBody>
       </Modal>
-      {/* <Button onClick={onOpen}>Open Modal</Button> */}
-      {/* <Modal isOpen={isOpen} onClose={onClose}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeaderStryled>Criar anuncio</ModalHeaderStryled>
-            <ModalCloseButton />
-            <ModalBody pb={6}>
-              <FormControl onSubmit={handleSubmit(onSubmitFunction)}>
-                <FormMain>
-                  <label className='label'>Tipo do anuncio</label>
-                  <Buttons>
-                      <button className='button1'>Venda</button>
-                      <button className='button2'>Leilão</button>
-                  </Buttons>
-                  <label className='label'>Informações de veículo</label>
-                  <Input label='Título' placeholder='Mercedes Benz A 200 CGI ADVANCE SEDAN'/>
-                  <InputsInfos>
-                      <Input label='Ano' placeholder='2018'/>
-                      <Input label='Quilometragem' placeholder='0'/>
-                      <Input label='Preço' placeholder='50.000.00'/>
-                  </InputsInfos>
-                  <Input  label='Descrição' placeholder='Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s'/> 
-                  <label className='label'>Tipo de veículo</label>
-                  <Buttons>
-                      <button className='button1'>Carro</button>
-                      <button className='button2'>Moto</button>
-                  </Buttons> 
-                  <label className='label'>Publicado</label>
-                  <Buttons>
-                      <button className='button1'>Sim</button>
-                      <button className='button2'>Não</button>
-                  </Buttons>    
-                  <Input label='Imagem de capa' placeholder='https://image.com'/>         
-                  <Input label='1º imagem da galeria' placeholder='https://image.com'/>         
-                  <Input label='2º imagem da galeria' placeholder='https://image.com'/>  
-                  <button id='adcCampo'>Adicionar campo para imagem da galeria</button> 
-                  <Buttons>
-                      <button className='button1'>Excluir anúncio</button>
-                      <button className='button2'>Salvar alterações</button>
-                  </Buttons>
-                </FormMain>      
-                </FormControl>
-             
-            </ModalBody>
-          </ModalContent>
-        </Modal> */}
     </>
- ) : null;
-} 
+  )
+}
 
 
 export default ModalForm
--- motors-shop-front/src/pages/sellerAd/SellerAd.tsx
+++ motors-shop-front/src/pages/sellerAd/SellerAd.tsx
@@ -15,9 +15,9 @@ const SellerAd = () => {
   const [productCart, setProductCart] = useState<IDataCard[]>([]);
   const [productMotorbike, setProductMotorbike] = useState<IDataCard[]>([]);
   const { modal, openModal } = useContext(AuthContext);
-  const { isOpen, onOpen } = useDisclosure(); 
+  const { isOpen, onOpen } = useDisclosure();
 
-  
+
   const userId = localStorage.getItem("userId");
   console.log(modal)
 
@@ -41,59 +41,61 @@ const SellerAd = () => {
   }, []);
 
   return (
-    <Conteiner>
-      {modal === true? <ModalForm /> : null} 
-      <Navbar />
-      <div className="backGroudHeader"></div>
-      <div className="advertiserCard">
-        <p className="seller">SL</p>
-        <div className="sellerConteiner">
-          <p className="Heading-6-600">Samuel Leão</p>{" "}
-          <p className="sellerType">Anunciante</p>
+    <>
+      {modal ? <ModalForm /> : null}
+      <Conteiner>
+        <Navbar />
+        <div className="backGroudHeader"></div>
+        <div className="advertiserCard">
+          <p className="seller">SL</p>
+          <div className="sellerConteiner">
+            <p className="Heading-6-600">Samuel Leão</p>{" "}
+            <p className="sellerType">Anunciante</p>
+          </div>
+          <p className="body-1-400 description">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text ever
+            since the 1500s
+          </p>
+          <button type='button' onClick={() => openModal()} className="big outlineBrand1">Criar anuncio</button>
         </div>
-        <p className="body-1-400 description">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s
-        </p>
-        <button type='button' onClick={() => openModal()} className="big outlineBrand1">Criar anuncio</button>
-      </div>
 
-      <h2 className="Heading-5-600 tipo" id="leilao">
-        leilão
-      </h2>
-      <ul className="leilaoConteiner">
-        <li>
-          <AuctionCard myAds={true} />
-        </li>
-        <li>
-          <AuctionCard myAds={true} />
-        </li>
-        <li>
-          <AuctionCard myAds={true} />
-        </li>
-      </ul>
+        <h2 className="Heading-5-600 tipo" id="leilao">
+          leilão
+        </h2>
+        <ul className="leilaoConteiner">
+          <li>
+            <AuctionCard myAds={true} />
+          </li>
+          <li>
+            <AuctionCard myAds={true} />
+          </li>
+          <li>
+            <AuctionCard myAds={true} />
+          </li>
+        </ul>
 
-      <h2 className="Heading-5-600 tipo" id="carros">
-        Carros
-      </h2>
-      <ProductCard
-        arrayProduto={productCart}
-        anuncianteCard={false}
-        myAds={true}
-      />
+        <h2 className="Heading-5-600 tipo" id="carros">
+          Carros
+        </h2>
+        <ProductCard
+          arrayProduto={productCart}
+          anuncianteCard={false}
+          myAds={true}
+        />
 
-      <h2 className="Heading-5-600 tipo" id="motos">
-        Motos
-      </h2>
-      <ProductCard
-        arrayProduto={productMotorbike}
-        anuncianteCard={false}
-        myAds={true}
-      />
+        <h2 className="Heading-5-600 tipo" id="motos">
+          Motos
+        </h2>
+        <ProductCard
+          arrayProduto={productMotorbike}
+          anuncianteCard={false}
+          myAds={true}
+        />
 
-      <Footer />
-    </Conteiner>
+        <Footer />
+      </Conteiner>
+    </>
   );
 };
 
